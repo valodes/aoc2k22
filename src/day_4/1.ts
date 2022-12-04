c62bcefb11232b8f6ba79465ad584c2d3ece9a6f
Valentin H <21902424@etu.unicaen.fr>
2022-12-04 19:16:32 +0100

    Refactor for better understandability

--- src/day_4/1.ts
+++ src/day_4/1.ts
@@ -1,27 +1,30 @@
 import { readFileSync } from 'fs';
 
 export const run = async () => {
+  // Read input and split into sections
+  const input = readFileSync('./src/day_4/input.txt', 'utf-8');
+  const sections = input.split('\n');
 
-    //On vient lire l'input et on le met dans une variable
-    const input = readFileSync('./src/day_4/input.txt', 'utf-8');
-    const sections = input.split('\n');
+  let overlap = 0;
+  let partialOverlap = 0;
 
-    let overlap = 0;
-    let partialOverlap = 0;
+  // Loop over each section and compare with the other sections
+  for (const section of sections) {
+    const pair = section.split(',');
+    const first = pair[0].split('-');
+    const second = pair[1].split('-');
 
-    //On boucle sur chaque section et on compare avec les autres
-    for(const section of sections) {
-        const pair = section.split(',');
-        const first = pair[0].split('-');
-        const second = pair[1].split('-');
+    // Destructure the start and end times for each section
+    const [aStart, aEnd, bStart, bEnd] = [+first[0], +first[1], +second[0], +second[1]];
 
-        const [as, ae, bs, be] = [+first[0], +first[1], +second[0], +second[1]];
+    // Check for overlap or partial overlap
+    if ((aStart <= bStart && aEnd >= bEnd) || (aStart >= bStart && aEnd <= bEnd)) {
+      overlap++;
+    }
+    if (aEnd >= bStart && bEnd >= aStart) {
+      partialOverlap++;
+    }
+  }
 
-        if ((as <= bs && ae >= be) || (as >= bs && ae <= be)) overlap++;
-        if (ae >= bs && be >= as) partialOverlap++;
-
-    }  
-
-    return { overlap, partialOverlap };
-
+  return { overlap, partialOverlap };
 };
